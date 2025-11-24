f212684a9fd409db6dd2e6df17e60346b05ebabe
maria.mejia <desarrollo@academicos.cesde.edu.co>
2025-11-24 15:07:52 -0500

    se hace cambio

---
+++ Poryecto integrado/controlador.js
@@ -0,0 +1,22 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento) {
+    evento.preventDefault()
+    
+
+    let cajaNombres=document.getElementById("cajanombres")
+    let cajaApellidos=document.getElementById("cajaapellidos")
+    let cajaFechaNacimiento=document.getElementById("cajafechanacimiento")
+    let cajaDireccion=document.getElementById("cajadireccion")
+    let cajaTelefono=document.getElementById("cajatelefono")
+    let datosDelFormulario={
+        nombres:cajaNombres.value,
+        apellidos:cajaApellidos.value,
+        fechanacimiento:cajaFechaNacimiento.value,
+        direccion:cajaDireccion.value,
+        telefono:cajaTelefono.value,
+        
+        
+    }
+
+
+})
